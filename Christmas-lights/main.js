b69dbc156ea7c7ae6860a3717fe3aaa25b28d6d8
Luke Logan <llogan@highpoint.edu>
2019-07-05 16:11:14 -0400

    adds christmas lights

---
+++ Christmas-lights/main.js
@@ -0,0 +1,49 @@
+// Select every li
+let lights = document.querySelectorAll('li');
+let evens = document.querySelectorAll('li:nth-child(even)');
+let odds = document.querySelectorAll('li:nth-child(odd)');
+const start = document.querySelector('.start');
+const stop = document.querySelector('.stop');
+
+let toggleEvens = setInterval(evenTimer, 1500);
+let toggleOdds = setInterval(oddTimer, 750);
+
+
+function evenTimer(){
+    console.log(toggleEvens);
+
+    for(even of evens){
+        even.classList.toggle('lights-on');
+    };
+}
+function oddTimer(){
+    for(odd of odds){
+        setTimeout(odd.classList.toggle('lights-on'), 750);
+    }
+};
+
+start.addEventListener('click', e =>{
+    setInterval(evenTimer, 1500);
+    setInterval(oddTimer, 750);
+
+} );
+
+stop.addEventListener('click', e => {
+    clearInterval(toggleEvens);
+    clearInterval(toggleOdds);
+
+
+});
+
+
+
+
+
+// function oddTimer(){
+//     for(even of evens){
+//         even.style.boxShadow = "inherit";
+//     }
+//     for(odd of odds){
+//         odd.style.boxShadow = "none";
+//     }
+// }
